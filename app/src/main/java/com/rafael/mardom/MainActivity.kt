e1b9bd81af23935168f36169009521b42e0d3922
RafaX8 <rafita8md@gmail.com>
2023-03-20 13:44:15 +0100

    Setting up project

--- app/src/main/java/com/rafael/mardom/MainActivity.kt
+++ app/src/main/java/com/rafael/mardom/MainActivity.kt
@@ -2,10 +2,26 @@ package com.rafael.mardom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rafael.mardom.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setUpBinding()
+        setupNavigation()
+    }
+
+    private fun setUpBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    private fun setupNavigation() {
+        // TODO
     }
 }
---
+++ app/src/main/java/com/rafael/mardom/app/presentation/AppApplication.kt
@@ -0,0 +1,8 @@
+package com.rafael.mardom.app.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppApplication : Application(){
+}
