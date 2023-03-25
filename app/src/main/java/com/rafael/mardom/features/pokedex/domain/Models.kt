086583b23e1f1032156e99101afe75c6c6b08885
RafaX8 <rafita8md@gmail.com>
2023-03-25 17:37:43 +0100

    pokedex list domain -> added more variables and data classes to file Models.kt

--- app/src/main/java/com/rafael/mardom/features/pokedex/domain/Models.kt
+++ app/src/main/java/com/rafael/mardom/features/pokedex/domain/Models.kt
@@ -3,11 +3,22 @@ package com.rafael.mardom.features.pokedex.domain
 data class Pokemon(
     val id: Int,
     val name: String,
+    val description: String,
+    val height: Double,
+    val weight: Double,
     val types: List<String>,
-    val stats: List<PokemonStats>
+    val stats: List<PokemonStats>,
+    val sprites: PokemonSprite,
 )
 
 data class PokemonStats(
     val name: String,
     val base: Int
+)
+
+data class PokemonSprite(
+    val front_default: String,
+    val front_shiny: String,
+    val back_default: String,
+    val back_shiny: String,
 )
