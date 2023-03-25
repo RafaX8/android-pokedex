5d4030968f0ed426f4bcb0ea615ac62aa3ddc9fd
RafaX8 <rafita8md@gmail.com>
2023-03-25 16:21:33 +0100

    pokedex list domain PokemonRepository

---
+++ app/src/main/java/com/rafael/mardom/features/pokedex/domain/Repositories.kt
@@ -0,0 +1,8 @@
+package com.rafael.mardom.features.pokedex.domain
+
+import com.rafael.mardom.app.domain.ErrorApp
+import com.rafael.mardom.app.domain.functional.Either
+
+interface PokemonRepository {
+    suspend fun getAll(): Either<ErrorApp, List<Pokemon>>
+}
