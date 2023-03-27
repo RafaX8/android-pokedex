cc08edeb60ef823d43a5a7ffc165629b2eba3e5d
RafaX8 <rafita8md@gmail.com>
2023-03-27 10:29:40 +0200

    PokemonDataRepository Update -> TODO: LocalDataSource

--- app/src/main/java/com/rafael/mardom/features/pokedex/data/PokemonDataRepository.kt
+++ app/src/main/java/com/rafael/mardom/features/pokedex/data/PokemonDataRepository.kt
@@ -2,15 +2,21 @@ package com.rafael.mardom.features.pokedex.data
 
 import com.rafael.mardom.app.domain.ErrorApp
 import com.rafael.mardom.app.domain.functional.Either
+import com.rafael.mardom.app.domain.functional.left
+import com.rafael.mardom.features.pokedex.data.remote.PokemonRemoteDataSource
 import com.rafael.mardom.features.pokedex.domain.Pokemon
 import com.rafael.mardom.features.pokedex.domain.PokemonRepository
 import javax.inject.Inject
 
 class PokemonDataRepository @Inject constructor(
     // private val localDataSource: PokemonLocalDataSource, // Será añadido en el futuro
-    // private val remoteDataSource: PokemonRemoteDataSource
+    private val remoteDataSource: PokemonRemoteDataSource
 ) : PokemonRepository {
     override suspend fun getAll(): Either<ErrorApp, List<Pokemon>> {
-        TODO("Not yet implemented")
+        return try {
+            remoteDataSource.getAll()
+        }catch (e: java.lang.Exception){
+            return ErrorApp.DataError.left()
+        }
     }
 }
